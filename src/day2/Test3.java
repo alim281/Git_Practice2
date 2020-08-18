7fde37fc0442102eba7d47cfb9e4de225d064dd4
alim281 <alim281@gmail.com>
2020-08-18 15:30:30 -0700

    updated

--- src/day2/Test3.java
+++ src/day2/Test3.java
@@ -3,6 +3,7 @@ package day2;
 public class Test3 {
     public static void main(String[] args) {
         System.out.println("Arda is a hadsome boy");
+        // This is test for practice
 
     }
 }
