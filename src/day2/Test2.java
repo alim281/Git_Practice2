4fd6c8403f7728c7250dd9193835abaf1cbaf90b
alim281 <68941969+alim281@users.noreply.github.com>
2020-08-18 15:13:11 -0700

    Update Test2.java

--- src/day2/Test2.java
+++ src/day2/Test2.java
@@ -2,6 +2,6 @@ package day2;
 
 public class Test2 {
     public static void main(String[] args) {
-        System.out.println("Alex");
+        System.out.println("Alex"); // this is for test
     }
 }
