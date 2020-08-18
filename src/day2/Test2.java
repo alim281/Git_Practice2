bcde786e66615a6b5b8acc9b438ec2967e4529e9
alim281 <alim281@gmail.com>
2020-08-18 15:20:18 -0700

    Test3 Class

--- src/day2/Test2.java
+++ src/day2/Test2.java
@@ -2,7 +2,6 @@ package day2;
 
 public class Test2 {
     public static void main(String[] args) {
-        System.out.println("Alex"); // this is for test
-        //this is for second test
+// This is test for 2 class
     }
 }
